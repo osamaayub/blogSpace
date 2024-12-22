2e6cedc075284af45958e81b0bcf78ff5eaff245
osamaayub <ayubosama416@gmail.com>
2024-12-22 16:45:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+let postsArray = []
+const titleInput = document.getElementById("post-title")
+const bodyInput = document.getElementById("post-body")
+const form=document.getElementById("new-post")
+const blogList=document.getElementById("blog-list")
+
+function renderPosts() {
+    let html = ""
+    for (let post of postsArray) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+    }
+    blogList.innerHTML = html
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        postsArray = data.slice(0, 5)
+        renderPosts()
+    })
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = titleInput.value
+    const postBody = bodyInput.value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
+            postsArray.unshift(post)
+            renderPosts()
+            /**
+             * Challenge: clear the form out!
+             */
+            titleInput.value = ""
+            bodyInput.value = ""
+        })
+})
